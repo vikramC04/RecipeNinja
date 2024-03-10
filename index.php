807351cf33b1de20bc428b7244b22d44cf9113e4
vikramC04 <139662459+vikramC04@users.noreply.github.com>
2024-03-09 22:37:28 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+    include('config/database_connect.php');
+    $sql = 'SELECT title, ingredients, id FROM recipe ORDER BY created_at';
+    $result = mysqli_query($conn, $sql);
+    $recipes = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_free_result($result);
+    mysqli_close($conn);
+
+?>
+<!DOCTYPE html>
+<html>
+	
+	<?php include('templates/header.php'); ?>
+
+	<h4 class="center grey-text">Recipes!</h4>
+
+	<div class="container">
+		<div class="row">
+
+			<?php foreach($recipes as $recipe): ?>
+
+				<div class="col s6 m4">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($recipe['title']); ?></h6>
+							<ul class="grey-text">
+								<?php foreach(explode(',', $recipe['ingredients']) as $items): ?>
+									<li><?php echo htmlspecialchars($items); ?></li>
+								<?php endforeach; ?>
+							</ul>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="info.php?id=<?php echo $recipe['id'] ?>">more info</a>
+						</div>
+					</div>
+				</div>
+
+			<?php endforeach; ?>
+
+		</div>
+	</div>
+
+	<?php include('templates/footer.php'); ?>
+
+</html>
+
---
+++ templates/footer.php
@@ -0,0 +1,4 @@
+<footer class="section">
+		<div class="center grey-text">&copy; Copyright 2024 Recipe Ninja</div>
+	</footer>
+</body>
